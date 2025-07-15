2585fc96387ca548933e758dcb2ce2e48ff536f3
Aariv <arivbits@gmail.com>
2025-07-15 23:05:27 +0530

    Updated Code

--- src/main/java/com/mydsa/recursion/Recursion_July15.java
+++ src/main/java/com/mydsa/recursion/Recursion_July15.java
@@ -6,12 +6,14 @@ import java.util.List;
 public class Recursion_July15 {
 
 	public static void main(String[] args) {
-		System.out.println(generateParenthesis(4));
+		System.out.println(generateParenthesis(3));
+		
 	}
 
 	public static List<String> generateParenthesis(int n) {
 	    List<String> result = new ArrayList<>();
-	    generate("", 0, 0, n, result);
+//	    generate("", 0, 0, n, result);
+	    generateP(0, 0, "", result, n);
 	    return result;
 	}
 
@@ -29,5 +31,21 @@ public class Recursion_July15 {
 	        generate(current + ")", open, close + 1, max, result);
 	    }
 	}
+	
+	private static void generateP(int open, int close, String s, List<String> result, int n) {
+		if(open == n && close == n) {
+			result.add(s);
+			return;
+		}
+		
+		if(open < n) {
+			generateP(open+1, close, s + "(", result, n);
+		}
+		
+		if(close < open) {
+			generateP(open, close + 1, s + ")", result, n);
+		}
+	}
+	
 
 }
