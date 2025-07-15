60076cd2788c42289b9cbd73aa14b9049c9c324e
Aariv <arivbits@gmail.com>
2025-07-15 23:54:35 +0530

    Binary String Generator

---
+++ src/main/java/com/mydsa/recursion/BinaryStringGenerator.java
@@ -0,0 +1,48 @@
+package com.mydsa.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryStringGenerator {
+
+	public static void generateBinaryStrings(int n) {
+//		generate("", n);
+		StringBuilder sb = new StringBuilder();
+		List<String> list = new ArrayList<String>();
+		generateB(sb, n, list);
+		System.out.println(list);
+	}
+
+	private static void generate(String current, int n) {
+		if (current.length() == n) {
+			System.out.println(current);
+			return;
+		}
+
+		generate(current + "0", n); // Add '0' and continue
+		generate(current + "1", n); // Add '1' and continue
+	}
+
+	public static void main(String[] args) {
+		int n = 3;
+		generateBinaryStrings(n);
+	}
+	
+	private static void generateB(StringBuilder sb, int n, List<String> list) {
+		if(sb.length() == n) {
+			list.add(sb.toString());
+			return;
+		}
+		
+		sb.append("0");
+		generateB(sb, n, list);
+		
+		// Remove
+		sb.deleteCharAt(sb.length()-1);
+		
+		sb.append("1");
+		generateB(sb, n, list);
+		// Remove
+		sb.deleteCharAt(sb.length()-1);
+	}
+}
