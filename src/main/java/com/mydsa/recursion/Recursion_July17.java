f954f8890f0aa626a7b10401a687968a0c9d1cf3
Aariv <arivbits@gmail.com>
2025-07-18 00:40:00 +0530

    Learnt Subset 2

--- src/main/java/com/mydsa/recursion/Recursion_July17.java
+++ src/main/java/com/mydsa/recursion/Recursion_July17.java
@@ -1,13 +1,14 @@
 package com.mydsa.recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Recursion_July17 {
 
 	public static void main(String[] args) {
 		// Subset
-		int[] arr = {1, 2, 3};
+		int[] arr = { 1, 2, 2 };
 		System.out.println(subSet(arr));
 	}
 
@@ -15,6 +16,7 @@ public class Recursion_July17 {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		List<Integer> innerArray = new ArrayList<Integer>();
 		int index = 0;
+		Arrays.sort(arr);
 		subSetHelper(arr, innerArray, result, index);
 		return result;
 	}
@@ -22,11 +24,15 @@ public class Recursion_July17 {
 	private static void subSetHelper(int[] arr, List<Integer> innerArray, List<List<Integer>> result, int index) {
 		innerArray = new ArrayList<Integer>(innerArray);
 		result.add(innerArray);
-		for(int i = index; i < arr.length; i++) {
+		for (int i = index; i < arr.length; i++) {
+			if (i > index && arr[i] == arr[i - 1]) {
+				continue;
+			}
 			innerArray.add(arr[i]);
 			subSetHelper(arr, innerArray, result, i + 1);
-			innerArray.remove(innerArray.size()-1);
+			innerArray.remove(innerArray.size() - 1);
+
 		}
 	}
-	
+
 }
